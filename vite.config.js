3606c6e32d1d71d27a63a47b5b0426beb0d6b4c7
Roshan-shahh <rs20150190128@gmial.com>
2025-09-07 20:25:02 +0530

    Initial project commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'https://gi-csb3.onrender.com'
-    }
-  }
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'safari >= 11'], // Safari ke liye force
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+    }),
+  ],
 });
